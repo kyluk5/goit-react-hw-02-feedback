b4bcd1eff3e2af71fe63cc990aa63140e9afd6be
Andrii Kulyk <kyluk5@gmail.com>
2020-07-15 01:04:46 +0300

    Updates

---
+++ precache-manifest.eb59cc71ab26545b464ace41035aa64d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1341e819e4f9c37ff677c531ae33696a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "dfecf363b9111312fdaa",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c50a29142d69adb05a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.5fcb013d.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
